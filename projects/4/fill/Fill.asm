c5aba7a0575640053e210a297b7d4cec2769500d
Smixi <sismixx@hotmail.fr>
2024-05-12 04:13:20 +0200

    add asm

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -9,3 +9,60 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// Loop until kbd event happens.
+(MAIN_LOOP)
+  // the screen is 256*512, 8192 continuous 16 bits words (32 words per row)
+  // Draw loop: increment the pixel index, set the pixel. Do that until we are at the end of the screen.
+  // Reset PIXEL_INDEX to screen to start drawing again
+  @SCREEN
+  D=A
+  @PIXEL_INDEX
+  M=D
+  // Check if kbd is pressed. If true, then we set the color to black, else to white
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+
+  @PIXEL_COLOR
+  M=-1
+  @DRAW_LOOP_FN
+  0;JMP
+
+(WHITE)
+  @PIXEL_COLOR
+  M=0
+  @DRAW_LOOP_FN
+  0;JMP
+
+(DRAW_LOOP_FN)
+(DRAW_LOOP)
+  @PIXEL_INDEX
+  //Check if we are done (PIXEL_INDEX = KBD)
+  D=M
+
+  // End of screen is 8192 (256*512) words.
+  @SCREEN
+  D=D-A
+  @8192
+  D=D-A
+  @MAIN_LOOP
+  D;JGE
+  
+  // Get the pixel color from the keyboard
+  @PIXEL_COLOR
+  D=M
+  //Get pixel index
+  @PIXEL_INDEX
+  A=M
+  //Draw the pixel with the color
+  M=D
+  @PIXEL_INDEX
+  //Increment pixel index
+  M=M+1
+  
+  @DRAW_LOOP
+  0;JMP
+
+
--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,29 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+  @2 // Load 0 in RAM 2 and set D to 0
+  M=0
+  @i // Loop variable
+  M=0
+(LOOP)
+  @i
+  D=M
+  @0 // We will loop R0 times
+  D=D-M
+  @END
+  D;JGE // (finished looping)
+
+  @1 // by adding R1 ot it
+  D=M
+  @2
+  M=D+M
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
+
+
